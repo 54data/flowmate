9fef4bf016219a004c7eb30461d7e2dc311c6856
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-18 15:53:13 +0900

    Add: CommonCodeDto 생성

---
+++ src/main/java/com/sailing/flowmate/dto/CommonCodeDto.java
@@ -0,0 +1,11 @@
+package com.sailing.flowmate.dto;
+
+import lombok.Data;
+
+@Data
+public class CommonCodeDto {
+	private int codeId;
+	private int codeTypeId;
+	private String codeName;
+	private int codeEnabled;
+}
