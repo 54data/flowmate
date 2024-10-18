9c1b034929813a2011f718961830179e042e9b03
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-18 15:52:36 +0900

    Add: LoginAuthenticationFailureHandler 생성

---
+++ src/main/java/com/sailing/flowmate/security/LoginAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package com.sailing.flowmate.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		setDefaultFailureUrl("/account/loginForm");
+		super.onAuthenticationFailure(request, response, exception);
+	}
+}
