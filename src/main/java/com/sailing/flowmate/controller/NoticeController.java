84d7f0ab073a2f621dc5e069793a552a3386f9ce
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-18 15:55:40 +0900

    Remove: service 관련 코드 삭제

--- src/main/java/com/sailing/flowmate/controller/NoticeController.java
+++ src/main/java/com/sailing/flowmate/controller/NoticeController.java
@@ -1,15 +1,10 @@
 package com.sailing.flowmate.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.sailing.flowmate.dto.NoticeDto;
-import com.sailing.flowmate.service.NoticeService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class NoticeController {
 	@Autowired
-	private NoticeService noticeService;
 	
 	@GetMapping("/noticeForm")
 	public String noticeForm() {
@@ -27,9 +21,11 @@ public class NoticeController {
 	
 	@GetMapping("/noticeList")
 	public String noticeList(String projectId, Model model){
-		
-		List<NoticeDto> allNotices = noticeService.getAllNotices(projectId);
-		model.addAttribute(allNotices);
 		return "notice/noticeList";
+	}
+	
+	@GetMapping("/noticeDetail")
+	public String noticeDetail(String projectId, Model model){
+		return "notice/noticeDetail";
 	}
 }
