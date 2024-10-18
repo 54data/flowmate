f84d2652e4f2eb83b9755ee4a7e203cbcf91e455
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-18 15:51:35 +0900

    Add: MemberDao 생성

---
+++ src/main/java/com/sailing/flowmate/dao/MemberDao.java
@@ -0,0 +1,17 @@
+package com.sailing.flowmate.dao;
+
+import com.sailing.flowmate.dto.MemberDto;
+
+public class MemberDao {
+
+	public MemberDto selectByUserId(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateMemberEnabled(MemberDto member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
