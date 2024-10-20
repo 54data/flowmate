384311b26f9c06e73d6581503a4a8c536bdb6497
54data <ehdvkf02@naver.com>
2024-10-21 02:28:02 +0900

    Add: 프로젝트 컨트롤러 추가

---
+++ src/main/java/com/sailing/flowmate/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.sailing.flowmate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/project")
+@Controller
+public class ProjectController {
+	@RequestMapping("/projectBoard")
+	public String projectBoard() {
+		return "project/projectBoard";
+	}
+}
