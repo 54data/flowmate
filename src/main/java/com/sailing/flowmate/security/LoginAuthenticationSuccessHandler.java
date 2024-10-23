b16932dfd9acdbe15c53f34e4550b4de2fa190db
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-23 23:54:25 +0900

    Add: 사용자 권한별 로그인 성공시 페이지 다르게 지정

--- src/main/java/com/sailing/flowmate/security/LoginAuthenticationSuccessHandler.java
+++ src/main/java/com/sailing/flowmate/security/LoginAuthenticationSuccessHandler.java
@@ -1,18 +1,36 @@
 package com.sailing.flowmate.security;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 public class LoginAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws ServletException, IOException {
-		super.onAuthenticationSuccess(request, response, authentication);
+/*		super.onAuthenticationSuccess(request, response, authentication);*/
+		
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        
+        for (GrantedAuthority authority : authorities) {
+            String role = authority.getAuthority();
+            if (role.equals("ROLE_ADMIN")) {
+                response.sendRedirect("/flowmate/mypage/adminPage");
+                return;
+            } else if (role.equals("ROLE_PM")) {
+                response.sendRedirect("/flowmate/");
+                return;
+            }
+        }
+        
+        response.sendRedirect("/");
+
 	}
 }
