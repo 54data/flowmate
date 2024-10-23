7453131f0fc428fcf309d657ce5587843a1c54aa
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-23 23:53:50 +0900

    Add: @Secured("ROLE_ADMIN") admin 관련 메서드에 추가

--- src/main/java/com/sailing/flowmate/controller/MypageController.java
+++ src/main/java/com/sailing/flowmate/controller/MypageController.java
@@ -1,5 +1,6 @@
 package com.sailing.flowmate.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,46 +18,58 @@ public class MypageController {
 		return "mypage/mypageMain";
 	}
 	
-	@GetMapping("/adminPage")
-	public String getAdminPage(){
-		
-		return "mypage/adminPage";
-	}
 	@GetMapping("/messageBox")
 	public String getMessageBox(){
 		
 		return "mypage/messageBox";
 	}
+	
 	@GetMapping("/messageDetail")
 	public String getMessageDetail(){
 		
 		return "mypage/messageDetail";
 	}
+	
+	
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/adminPage")
+	public String getAdminPage(){
+		
+		return "mypage/adminPage";
+	}
+
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/adminPageDisable")
 	public String getAdminPageDisable(){
 		
 		return "mypage/adminPageDisable";
 	}
+	
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/adminPageStay")
 	public String getAdminPageStay(){
 		
 		return "mypage/adminPageStay";
 	}
+	
 	@GetMapping("/myIssue")
 	public String getMyIssue(){
 		
 		return "mypage/myIssue";
 	}
+	
 	@GetMapping("/myProject_Dev")
 	public String getMyProjectDev(){
 		
 		return "mypage/myProject_Dev";
 	}
+	
 	@GetMapping("/myProject")
 	public String getMyProject(){
 		
 		return "mypage/myProject";
 	}
+	
 	@GetMapping("/myTask")
 	public String getMyTask(){
 		
