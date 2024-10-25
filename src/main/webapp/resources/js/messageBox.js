b498af200688c99a9e103145506aa993704b5376
AnJungGeon <gunn0128@gmail.com>
2024-10-25 23:29:23 +0900

    Feat: 쪽지 검색 / 읽음 표시 추가
    쪽지 검색과 읽음 표시 하면 아이콘이 변경되는 효과가 나타게 우선 정적으로 구현했습니다.

---
+++ src/main/webapp/resources/js/messageBox.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(document).on('click', '.send', function() {
+		window.open('/flowmate/message/messageSend','_blank', 'width=600, height=480, scrollbars=no');
+    });
+
+});		
