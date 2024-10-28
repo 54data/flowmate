dc163fa398a89a471aaa58fd675676318c581551
AnJungGeon <gunn0128@gmail.com>
2024-10-28 22:28:56 +0900

    Add: 작업 dto 생성
    작업 데이터와 관련된 dto를 생성했습니다.

---
+++ src/main/java/com/sailing/flowmate/dto/TaskDto.java
@@ -0,0 +1,31 @@
+package com.sailing.flowmate.dto;
+
+import lombok.Data;
+
+@Data
+public class TaskDto {
+	
+	private String taskId;
+	private String projectId;
+	private String memberId;
+	private String taskStep;
+	private String taskName;
+	private String taskPriority;
+	private String taskState;
+	private String taskStartDate;
+	private String taskDueDate;
+	private String taskEndDate;
+	private String taskContent;
+	private String taskRegdate;
+	private boolean taskEnabled;
+	private String stepStartDate;
+	private String stepDueDate;
+	private String taskUpdateDate;
+	private String taskUpdateMid;
+	private String taskLog;
+	
+	private String fileId;
+	private String fileName;
+	private String fileType;
+	private byte[] fileData;	
+}
