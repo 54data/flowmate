c2c2fa3099eeeee8c436d2887cdf70c5cd3db8df
Yerin_Hwang <babybbmm59@gmail.com>
2024-10-29 09:55:59 +0900

    Remove: 불필요한 코드 삭제

--- src/main/java/com/sailing/flowmate/controller/HomeController.java
+++ src/main/java/com/sailing/flowmate/controller/HomeController.java
@@ -8,14 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@Slf4j
 public class HomeController {
 	
 	@RequestMapping("")
 	public String getMypageMain(Authentication authentication, Model model) {
 	    if(authentication != null) {
 	    	String userName = authentication.getName();
-	    	log.info("실행" + userName);
 	    	model.addAttribute("userName", userName);
 	    }
 		return "mypageMain";
