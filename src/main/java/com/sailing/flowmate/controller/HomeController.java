d395503cd2c944278a14747b817a5c70848ba3f4
54data <ehdvkf02@naver.com>
2024-10-30 12:47:19 +0900

    Feat: 마이페이지 메인 나의 프로젝트 목록 불러오기 구현

--- src/main/java/com/sailing/flowmate/controller/HomeController.java
+++ src/main/java/com/sailing/flowmate/controller/HomeController.java
@@ -1,22 +1,34 @@
 package com.sailing.flowmate.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.slf4j.Slf4j;
+import com.sailing.flowmate.dto.ProjectDto;
+import com.sailing.flowmate.service.ProjectService;
 
 @Controller
 public class HomeController {
+	@Autowired
+	ProjectService projectService;
 	
 	@RequestMapping("")
 	public String getMypageMain(Authentication authentication, Model model) {
-	    if(authentication != null) {
-	    	String userName = authentication.getName();
-	    	model.addAttribute("userName", userName);
-	    }
+		if (authentication != null) {
+	    	String memberId = authentication.getName();
+	    	model.addAttribute("userName", memberId);
+	    	List<ProjectDto> myProjectsList = getMyProjects(memberId);
+	    	model.addAttribute("myProjectsList", myProjectsList);
+		}
 		return "mypageMain";
 	}
-
+	
+	public List<ProjectDto> getMyProjects(String memberId) {
+		List<ProjectDto> myProjectsList = projectService.getMyProjectsList(memberId);
+		return myProjectsList;
+	}
 }
