891072856c9439815467dec6403c3f7186e35175
54data <ehdvkf02@naver.com>
2024-11-04 01:33:09 +0900

    Feat: 프로젝트 수정 모달 기존 파일 삭제 및 신규 파일 입력 기능 구현

--- src/main/java/com/sailing/flowmate/dao/FilesDao.java
+++ src/main/java/com/sailing/flowmate/dao/FilesDao.java
@@ -2,6 +2,7 @@ package com.sailing.flowmate.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.sailing.flowmate.dto.FilesDto;
 import com.sailing.flowmate.dto.TaskDto;
@@ -16,5 +17,6 @@ public interface FilesDao {
 	public List<FilesDto> selectTaskAttach(String relatedId);
 
 	public int deleteTaskAttach(String fileId);
-
+	
+	public void deleteProjectFileData(@Param("projectId") String projectId, @Param("fileIdList") List<String> fileIdList);
 }
