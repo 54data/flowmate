c34743ec5210dc569591c169bf4aeb39a7bd3041
AnJungGeon <gunn0128@gmail.com>
2024-11-04 20:00:13 +0900

    Fix: datePicker 한글화
    datePicker 한글화했습니다.

--- src/main/webapp/resources/js/taskCreating.js
+++ src/main/webapp/resources/js/taskCreating.js
@@ -126,18 +126,29 @@ $(document).ready(function() {
     
     // 작업 기간 설정
     $('.task-date-range').daterangepicker(
-        {
-            locale: {
-                format: 'YYYY/MM/DD'
-            },
-            startDate: moment(),
-            endDate: moment(),
-        }, 
-        function(start, end) {
-            $('#taskStartDate').val(start.format('YYYYMMDDHHmmss'));
-            $('#taskDueDate').val(end.format('YYYYMMDDHHmmss'));
-        }
-    );
+    	    {
+    	        locale: {
+    	            format: 'YYYY-MM-DD',
+    	            separator: " ~ ",
+    	            applyLabel: "확인",
+    	            cancelLabel: "취소",
+    	            fromLabel: "From",
+    	            toLabel: "To",
+    	            customRangeLabel: "Custom",
+    	            weekLabel: "W",
+    	            daysOfWeek: ["일", "월", "화", "수", "목", "금", "토"],
+    	            monthNames: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    	            firstDay: 1
+    	        },
+    	        drops: "auto",
+    	        startDate: moment(),
+    	        endDate: moment()
+    	    },
+    	    function(start, end) {
+    	        $('#taskStartDate').val(start.format('YYYYMMDDHHmmss'));
+    	        $('#taskDueDate').val(end.format('YYYYMMDDHHmmss'));
+    	    }
+    	);
 
     
     $('.task-step-date-range').daterangepicker(
