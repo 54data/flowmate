1be546646d3150ae59858b43f617ec8d87c380a8
AnJungGeon <gunn0128@gmail.com>
2024-11-05 01:40:49 +0900

    Feat: 마이페이지 작업목록 추가
    마이페이지 오늘 작업, 지연 작업 목록을 불러오기 위한 요청 메소드를 추가했습니다.

--- src/main/java/com/sailing/flowmate/controller/HomeController.java
+++ src/main/java/com/sailing/flowmate/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.sailing.flowmate.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,26 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sailing.flowmate.dto.ProjectDto;
+import com.sailing.flowmate.dto.TaskDto;
 import com.sailing.flowmate.service.ProjectService;
+import com.sailing.flowmate.service.TaskService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 @Secured("ROLE_DEV")
 public class HomeController {
 	@Autowired
 	ProjectService projectService;
+	
+	@Autowired
+	TaskService taskService;
 	
 	@RequestMapping("")
 	public String getMypageMain(Authentication authentication, Model model) {
@@ -24,7 +35,9 @@ public class HomeController {
 	    	String memberId = authentication.getName();
 	    	model.addAttribute("userName", memberId);
 	    	List<ProjectDto> myProjectsList = getMyProjects(memberId);
+
 	    	model.addAttribute("myProjectsList", myProjectsList);
+
 		}
 		return "mypageMain";
 	}
@@ -32,5 +45,22 @@ public class HomeController {
 	public List<ProjectDto> getMyProjects(String memberId) {
 		List<ProjectDto> myProjectsList = projectService.getMyProjectList(memberId);
 		return myProjectsList;
+	}
+	
+
+	@GetMapping("/myTasks")
+	public String getTasksFragment(@RequestParam("type") String type, Authentication authentication, Model model) {
+	    String memberId = authentication.getName();
+	    List<TaskDto> tasks = new ArrayList<>();
+
+	    
+	    if ("today".equals(type)) {
+	        tasks = taskService.getMyTaskListForHome(memberId); 
+	    } else if ("delayed".equals(type)) {
+	        tasks = taskService.getMyDelayTask(memberId);
+	    }
+
+	    model.addAttribute("tasks", tasks);
+	    return "mypage/myTaskList";
 	}
 }
