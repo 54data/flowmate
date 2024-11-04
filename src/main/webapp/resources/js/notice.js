b2d0408d7337b6ea18953dae750a9723bbbd0008
Yerin_Hwang <babybbmm59@gmail.com>
2024-11-04 20:58:14 +0900

    Add: 첨부 파일 복수로 담아오도록 수정, 업데이트시 removeFile()한 파일은 삭제하도록 변경

--- src/main/webapp/resources/js/notice.js
+++ src/main/webapp/resources/js/notice.js
@@ -34,32 +34,6 @@ function validateForm(){
 	}	
 }
 
-$('#noticeTitle').keyup(function (e){
-	var content = $(this).val();
-	$('#titleLength').html("("+content.length+"/50)");
-	
-	if(content.length > 50){
-		Toast.fire({
-		    icon: 'error',
-		    title: '제목 글자수가 50자를 초과하였습니다.'
-		});
-		return false;
-	}
-})
-
-$('#noticeContent').keyup(function (e){
-	var content = $(this).val();
-	$('#contentsLength').html("("+content.length+"/2000)");
-	
-	if(content.length > 2000){
-		Toast.fire({
-		    icon: 'error',
-		    title: '내용 글자수가 2000자를 초과하였습니다.'
-		});
-		return false;
-	}
-})
-
 $(document).ready(function() {
 	$(document).on('click', '.notice-file-input-btn', function() {
 		$('.notice-file-input').trigger('click');
@@ -67,7 +41,172 @@ $(document).ready(function() {
 	
 	noticeHandler.init();
 	noticeHandler.removeFile();
+	
+	/*유효성 검사*/
+	$('#noticeTitle').keyup(function (e){
+		var content = $(this).val();
+		$('#titleLength').html("("+content.length+"/50)");
+		
+		if(content.length > 50){
+			Toast.fire({
+			    icon: 'error',
+			    title: '제목 글자수가 50자를 초과하였습니다.'
+			});
+			return false;
+		}
+	});
+
+	$('#noticeContent').keyup(function (e){
+		var content = $(this).val();
+		$('#contentsLength').html("("+content.length+"/2000)");
+		
+		if(content.length > 2000){
+			Toast.fire({
+			    icon: 'error',
+			    title: '내용 글자수가 2000자를 초과하였습니다.'
+			});
+			return false;
+		}
+	});
+
+	/*공지사항 등록*/
+	$('#noticeInsert-btn').on('click', function(event) {
+	    event.preventDefault(); 
+	    var formData = new FormData();
+	    
+	    const projectId = $(this).data("project-id");
+	    
+	    var noticeAttaches = $('#noticeAttach')[0].files;
+	    
+	    console.log('선택된 파일 개수:', noticeAttaches.length);
+	    	    
+	    for (var i = 0; i < noticeAttaches.length; i++) {
+	        formData.append('noticeAttaches', noticeAttaches[i]);
+	    }
+	    
+	    formData.append('noticeTitle', $('#noticeTitle').val());
+	    formData.append('noticeContent', $('#noticeContent').val());
+	    formData.append('projectId', projectId);
+	    
+	    $.ajax({
+	        url: '/flowmate/notice/insertNotice', 
+	        type: 'POST', 
+	        data: formData,
+	        processData: false,
+	        contentType: false,
+	        success: function(response) {
+	            console.log('공지사항 등록 성공');
+	            console.log("선택된 파일 수: " + noticeAttaches.length);
+	            console.log('파일 업로드 성공:', response);
+	            Toast.fire({
+	    		    icon: 'success',
+	    		    title: '등록을 성공하였습니다.'
+	    		});
+
+	            setTimeout(function() {
+	                window.location.href = '/flowmate/notice/noticeList?projectId='+projectId+'&pageNo=1';
+	            }, 2500);
+	        },
+	        error: function(error) {
+	            console.error('공지사항 등록 실패');
+	            console.error(error);
+	        }
+	    });
+	});
+
+	/*공지사항 수정*/
+	$('#noticeUpdate-btn').on('click', function(event) {
+	    event.preventDefault(); 
+	    var formData = new FormData();
+	    const projectId = $(this).data("project-id");
+	    const noticeId = $(this).data("notice-id");
+	    
+	    var noticeAttaches = $('#noticeUpdateAttach')[0].files;
+	    for (var i = 0; i < noticeAttaches.length; i++) {
+	        formData.append('noticeAttaches', noticeAttaches[i]);
+	    }
+
+	    deletedFileIds.forEach(id => formData.append('existingFileIds', id));
+
+	    formData.append('noticeTitle', $('#noticeUpdateTitle').val());
+	    formData.append('noticeContent', $('#noticeUpdateContent').val());
+	    formData.append('projectId', projectId);
+	    formData.append('noticeId', noticeId);
+	    
+	    $.ajax({
+	        url: '/flowmate/notice/updateNotice', 
+	        type: 'POST', 
+	        data: formData,
+	        processData: false,
+	        contentType: false,
+	        success: function(response) {
+	            console.log('공지사항 수정 성공');
+	            console.log("선택된 파일 수: " + noticeAttaches.length);
+
+	            Toast.fire({
+	    		    icon: 'success',
+	    		    title: '수정을 성공하였습니다.'
+	    		});
+
+	            setTimeout(function() {
+	                window.location.href = '/flowmate/notice/noticeList?projectId='+projectId+'&pageNo=1';
+	            }, 2500);
+	        },
+	        error: function(error) {
+	            console.error('공지사항 등록 실패');
+	            console.error(error);
+	        }
+	    });
+	});
+
+	/*공지사항 비활성화*/
+	$('#noticeDisable-btn').on('click', function(event) {
+	    event.preventDefault(); 
+	    
+	    const projectId = $(this).data("project-id");
+	    const noticeId = $(this).data("notice-id");
+	    
+		Swal.fire({
+		    title: '정말 비활성하시겠습니까?',
+		    text: "이 작업은 되돌릴 수 없습니다.",
+		    icon: 'warning',
+		    showCancelButton: true,
+		    confirmButtonColor: '#3085d6',
+		    cancelButtonColor: '#d33',
+		    confirmButtonText: '네, 비활성화할게요!',
+		    cancelButtonText: '아니요, 취소할게요!'
+		}).then((result) => {
+		    if (result.isConfirmed) {
+			    $.ajax({
+			        url: '/flowmate/notice/enabledNotice', 
+			        type: 'POST', 
+			        data: { noticeId: noticeId,
+			        	projectId: projectId},
+			        success: function(response) {
+			            console.log('공지사항 비활성화 성공');
+
+			            Toast.fire({
+			    		    icon: 'success',
+			    		    title: '공지사항이 비활성화되었습니다.'
+			    		});
+			            setTimeout(function() {
+			                window.location.href = '/flowmate/notice/noticeList?projectId='+projectId+'&pageNo=1';
+			            }, 2500);
+			        },
+			        error: function(error) {
+			            console.error('공지사항 비활성화 실패');
+			            console.error(error);
+			        }
+			    });
+		    } else {
+		        console.log("삭제가 취소되었습니다.");
+		        }
+		    });
+	});
+
 })
+
+let deletedFileIds = [];
 
 const noticeHandler = {
 	init() {
@@ -92,6 +231,8 @@ const noticeHandler = {
 			if (!$(e.target).hasClass('file-remove')) return;
 			const removeTargetId = $(e.target).data('index');
 			const removeTarget = $('#' + removeTargetId);
+			deletedFileIds.push(removeTargetId);
+			
 			const files = $('.notice-file-input')[0].files;
 			const dataTransfer = new DataTransfer();
 			
@@ -106,40 +247,3 @@ const noticeHandler = {
 	}
 };
 
-$('#insertBtn').on('click', function(event) {
-    event.preventDefault(); 
-    var formData = new FormData();
-
-    var noticeAttaches = $('#noticeAttach')[0].files;
-    for (var i = 0; i < noticeAttaches.length; i++) {
-        formData.append('noticeAttach', noticeAttaches[i]);
-    }
-
-    formData.append('noticeTitle', $('#noticeTitle').val());
-    formData.append('noticeContent', $('#noticeContent').val());
-    
-    $.ajax({
-        url: '/flowmate/notice/insertNotice', 
-        type: 'POST', 
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(response) {
-            console.log('공지사항 등록 성공');
-            console.log("선택된 파일 수: " + noticeAttaches.length);
-
-            Toast.fire({
-    		    icon: 'success',
-    		    title: '등록을 성공하였습니다.'
-    		});
-
-            setTimeout(function() {
-                window.location.href = '/flowmate/notice/noticeList?pageNo=1';
-            }, 2500);
-        },
-        error: function(error) {
-            console.error('공지사항 등록 실패');
-            console.error(error);
-        }
-    });
-});
