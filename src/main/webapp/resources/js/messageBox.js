f62b189aa82c17ff8f37dcd959646db584132a23
AnJungGeon <gunn0128@gmail.com>
2024-11-07 02:25:43 +0900

    Feat: 쪽지 팝업 수정
    쪽지 팝업 첨부파일 갯수 올라가게 수정 했습니다.

--- src/main/webapp/resources/js/messageBox.js
+++ src/main/webapp/resources/js/messageBox.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(document).on('click', '.send', function() {
-		window.open('/flowmate/message/messageSend','_blank', 'width=600, height=480, scrollbars=no');
+		window.open('/flowmate/message/messageSend','_blank', 'width=600, height=500, scrollbars=yes ');
     });
 
 });		
