5c7ccca9a1db9e65081676f1f8262737078d973f
54data <ehdvkf02@naver.com>
2024-11-06 09:15:43 +0900

    Add: 참여 프로젝트 페이지 datatables 관련 스크립트 추가

--- src/main/webapp/resources/js/myProject.js
+++ src/main/webapp/resources/js/myProject.js
@@ -1,130 +1,64 @@
-$.extend($.fn.dataTable.defaults, {
-	lengthChange: false,
-	language: {
-	    emptyTable: '데이터가 없습니다',
-	    info: '_START_ - _END_ / _TOTAL_',
-	    infoEmpty: '0 - 0 / 0',
-	    infoFiltered: '(총 _MAX_ 개)',
-	    infoThousands: ',',
-	    lengthMenu: '페이지당 줄수 _MENU_',
-	    loadingRecords: '읽는중...',
-	    processing: '처리중...',
-	    search: '검색:',
-	    zeroRecords: '검색 결과가 없습니다',
-	    paginate: {
-	        first: '처음',
-	        last: '마지막',
-	        next: '다음',
-	        previous: '이전'
-	    },
-	    aria: {
-	        sortAscending: ': 오름차순 정렬',
-	        sortDescending: ': 내림차순 정렬'
-	    },
-	    buttons: {
-	        copyKeys: 'ctrl키 나 u2318 + C키로 테이블 데이터를 시스템 복사판에서 복사하고 취소하려면 이 메시지를 클릭하거나 ESC키를 누르면 됩니다.',
-	        copySuccess: {
-	            _: '%d행을 복사판에서 복사됨',
-	            1: '1행을 복사판에서 복사됨'
-	        },
-	        copyTitle: '복사판에서 복사',
-	        csv: 'CSV',
-	        pageLength: {
-	            '-1': '모든 행 보기',
-	            _: '%d행 보기'
-	        },
-	        pdf: 'PDF',
-	        print: '인쇄',
-	        collection: '집합 <span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"></span>',
-	        colvis: '컬럼 보기',
-	        colvisRestore: '보기 복원',
-	        copy: '복사',
-	        excel: '엑셀'
-	    },
-	    searchBuilder: {
-	        add: '조건 추가',
-	        button: {
-	            0: '빌더 조회',
-	            _: '빌더 조회(%d)'
-	        },
-	        clearAll: '모두 지우기',
-	        condition: '조건',
-	        data: '데이터',
-	        deleteTitle: '필터 규칙을 삭제',
-	        logicAnd: 'And',
-	        logicOr: 'Or',
-	        title: {
-	            0: '빌더 조회',
-	            _: '빌더 조회(%d)'
-	        },
-	        value: '값'
-	    },
-	    autoFill: {
-	        cancel: '취소',
-	        fill: '모든 셀에서 <i>%d<i>을(를) 삽입</i></i>',
-	        fillHorizontal: '수평 셀에서 값을 삽입',
-	        fillVertical: '수직 셀에서 값을 삽입'
-	    },
-	    datetime: {
-	        previous: '이전',
-	        next: '다음',
-	        hours: '시',
-	        minutes: '분',
-	        seconds: '초',
-	        unknown: '-',
-	        amPm: [
-	            '오전',
-	            '오후'
-	        ],
-	        weekdays: [
-	            '일',
-	            '월',
-	            '화',
-	            '수',
-	            '목',
-	            '금',
-	            '토'
-	        ],
-	        months: [
-	            '1월',
-	            '2월',
-	            '3월',
-	            '4월',
-	            '5월',
-	            '6월',
-	            '7월',
-	            '8월',
-	            '9월',
-	            '10월',
-	            '11월',
-	            '12월'
-	        ]
-	    },
-	    editor: {
-	        close: '닫기',
-	        create: {
-	            button: '추가',
-	            title: '항목 추가',
-	            submit: '완료'
-	        },
-	        edit: {
-	            button: '수정',
-	            title: '항목 수정',
-	            submit: '완료'
-	        },
-	        remove: {
-	            button: '삭제',
-	            title: '항목 삭제',
-	            submit: '완료'
-	        },
-	        error: {
-	            system: '에러가 발생하였습니다 (&lt;a target="\\" rel="nofollow" href="\\"&gt;자세한 정보&lt;/a&gt;).'
-	        }
-	    }
-	}
-});
 $(document).ready(function() {
-	$('#projectList').DataTable({
-		searching: false,
+    const table = $('#projectList').DataTable({
+		order: [0, 'desc'],
+		orderClasses: true,
+		initComplete: function() {
+	        this.api()
+            .columns([8]) // '상태' 열에만 지정
+            .every(function () {
+                let column = this;
+                let dropdown = $('.dropdown-menu');
+                dropdown.append(`<li><a class="dropdown-item" href="#">전체</a></li>`);
+                // 해당 열의 유니크 값들을 드롭다운 옵션으로 지정
+                column
+                    .data()
+                    .unique()
+                    .sort()
+                    .each(function (d) {
+                    	dropdown.append(`<li><div class="dropdown-item" data-value="${d}">${d}</div></li>`);
+                    });
+                // 드롭다운 옵션을 선택했을 때 필터링된 행만 나오도록 이벤트 추가
+                dropdown.on('click', '.dropdown-item', function () {
+                	const dropdownVal = $(this).data('value');
+                	if (dropdownVal == '전체') {
+                		column.search('').draw();
+                	} else {
+	                    column
+	                        .search(dropdownVal ? dropdownVal : '', true, false)
+	                        .draw();
+                	}
+                });
+            });
+		},
+		columnDefs: [
+			{
+				targets: [0], 
+				render: function(data, type, row) {
+				    if (type === 'sort' || type === 'type') {
+				        return parseInt(data.split('-')[1], 10);
+				    }
+				    return data;
+				}
+			},
+			{targets: [1], orderable: false},
+			{targets: [2], orderable: false},
+			{
+				targets: [6], 
+				render: function(data, type, row) {
+				    if (type === 'sort' || type === 'type') {
+				        return parseInt(data, 10);
+				    }
+				    return data;
+				}
+			},
+			{targets: [7], type: 'num-fmt'},
+			{targets: [8], orderable: false},
+		],
+        createdRow: function(row, data, dataIndex) {
+            $(row).on('click', function() {
+                const projectId = data[0];
+                window.location.href = '../../flowmate/project/projectBoard?projectId=' + projectId;
+            });
+        }
 	});
 });
