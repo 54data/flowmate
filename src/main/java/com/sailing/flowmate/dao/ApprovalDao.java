50365ee53174828ce851271c4cc2d423ef0f3602
Yerin_Hwang <babybbmm59@gmail.com>
2024-11-07 17:02:57 +0900

    Feat: projectId로 결재 테이블에서 해당행을 select해오는 dao 추가

--- src/main/java/com/sailing/flowmate/dao/ApprovalDao.java
+++ src/main/java/com/sailing/flowmate/dao/ApprovalDao.java
@@ -1,5 +1,7 @@
 package com.sailing.flowmate.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.sailing.flowmate.dto.ApprovalDto;
@@ -10,5 +12,7 @@ public interface ApprovalDao {
 	public int selectApprNewNo();
 
 	public void insertApprData(ApprovalDto apprData);
+
+	public List<ApprovalDto> getApprovalList(String projectId);
 
 }
