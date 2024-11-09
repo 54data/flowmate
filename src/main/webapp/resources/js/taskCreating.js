50235562ee668d7d639271ec6c038fbbcae9172d
54data <ehdvkf02@naver.com>
2024-11-09 16:45:03 +0900

    Add: 작업 모달창에 이슈 추가 버튼 활성화되도록 스크립트 추가

--- src/main/webapp/resources/js/taskCreating.js
+++ src/main/webapp/resources/js/taskCreating.js
@@ -260,10 +260,28 @@ $(document).ready(function() {
         
         const taskIdForUpdate = $(this).data('task-id');
         openTaskUpdateModal(taskIdForUpdate, projectId)
+        
+    	$('.task-add-issue').on('click', function() {
+    	    $('.show-issue-modal').data('triggeredBy', $(this).data('issueMode'));
+    	    $('.show-issue-modal').data('taskId', taskId);
+    	    $('.show-issue-modal').trigger('click');
+        });
+        
+    	$(document).on('click', '.issue-id', function() {
+    		$('.show-issue-modal').data('triggeredBy', 'read');
+    		$('.show-issue-modal').data('issueId', $(this).data('issueId'));
+    		$('.show-issue-modal').trigger('click');
+    	});
+    	
+    	$(document).on('click', '.issue-title', function() {
+    		const issueId = $(this).data('issueId');
+    		$('.show-issue-modal').data('triggeredBy', 'read');
+    		$('.show-issue-modal').data('issueId', $(this).data('issueId'));
+    		$('.show-issue-modal').trigger('click');
+    	});
     });
 
     function openTaskUpdateModal(taskId, projectId){
-    		
         taskHandler.taskInit(true); // 모달 초기화
         $('.task-date-range').val('');
         $(".task-name").val("");
