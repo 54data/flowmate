313ef7322f9d26fdde719d4e4578523116fe53fc
54data <ehdvkf02@naver.com>
2024-11-11 02:34:42 +0900

    Fix: 권한 컬럼 임시 복원

--- src/main/webapp/resources/js/projectMemberManage.js
+++ src/main/webapp/resources/js/projectMemberManage.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 		orderClasses: true,
 		columns: columns,
 		initComplete: function() {
-		    const columnsToApplyFilter = [2, 3, 4]; 
+		    const columnsToApplyFilter = [2, 3, 5]; 
 
 		    columnsToApplyFilter.forEach((columnIndex) => {
 		        this.api()
@@ -62,6 +62,9 @@ $(document).ready(function() {
 		                    dropdownId = 'projectMemberManageRank';
 		                    break;
 		                case 4:
+		                    dropdownId = 'projectMemberManageRole';
+		                    break;
+		                case 5:
 		                    dropdownId = 'projectMemberManageEnabled';
 		                    break;
 		            }
@@ -88,7 +91,7 @@ $(document).ready(function() {
 		    });
 		},
 		columnDefs: [
-			{targets: [0, 2, 3, 4, 5], orderable: false},
+			{targets: [0, 2, 3, 4, 5, 6], orderable: false},
 		],
 	});
     
