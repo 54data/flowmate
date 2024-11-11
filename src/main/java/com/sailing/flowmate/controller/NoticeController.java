adeabcbb06f9108d28c92215495db5c19d076cca
Yerin_Hwang <babybbmm59@gmail.com>
2024-11-11 22:23:20 +0900

    Remove: 불필요한 코드 삭제

--- src/main/java/com/sailing/flowmate/controller/NoticeController.java
+++ src/main/java/com/sailing/flowmate/controller/NoticeController.java
@@ -2,15 +2,11 @@ package com.sailing.flowmate.controller;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
