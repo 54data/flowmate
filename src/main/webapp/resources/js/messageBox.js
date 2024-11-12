9122a99d7f64822735fa403aaada0b2a80f784f4
AnJungGeon <gunn0128@gmail.com>
2024-11-12 11:07:58 +0900

    Fix: 쪽지 내용에서 답장 팝업
    쪽지 내용 부분에서 답장 팝업 제대로 나오지 않는 부분 수정했습니다.

--- src/main/webapp/resources/js/messageBox.js
+++ src/main/webapp/resources/js/messageBox.js
@@ -4,15 +4,17 @@ $(document).ready(function() {
     });
     
     $(document).on('click','.reply', function() {
-        const receiverId = $(this).closest('.message').find('.sender-id').text().trim(); 
+        let receiverId = $(this).closest('.message').find('.sender-id').text().trim(); 
         
-        const originalContent = $(this).closest('.message').find('.messageContext').text().trim();
+        let originalContent = $(this).closest('.message').find('.messageContext').text().trim();
 
-        const popup = window.open(`/flowmate/message/messageSend?receiverId=${receiverId}&originalContent=${encodeURIComponent(originalContent)}`, '_blank', 'width=600, height=500, scrollbars=yes');
+        window.open(`/flowmate/message/messageSend?receiverId=${receiverId}&originalContent=${encodeURIComponent(originalContent)}`, '_blank', 'width=600, height=500, scrollbars=yes');
     });
     $(document).on('click','.md-reply', function() {
-    	const receiverId = $('.senderId').text().trim(); 
-    	window.open(`/flowmate/message/messageSend?receiverId=${receiverId}`, '_blank', 'width=600, height=500, scrollbars=yes');
+	    	let receiverId = $('.senderId').text().trim(); 
+	    	
+	    	let originalContent = $('.messageDetail').text().trim();	
+		window.open(`/flowmate/message/messageSend?receiverId=${receiverId}&originalContent=${encodeURIComponent(originalContent)}`, '_blank', 'width=600, height=500, scrollbars=yes');
     });
     $(document).on('click','.showList', function() {
     	location.href = "/flowmate/message/messageBox"
